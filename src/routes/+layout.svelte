4fa549d32b55835b43e1a305c91f5bc0cf07c3d3
Romil <khandryga.romil@gmail.com>
2023-10-16 22:25:42 +0300

    view transition experimental

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,62 @@
 <script>
-	import Header from './Header.svelte';
-	import './styles.css';
+  import { onNavigate } from '$app/navigation'
+  import Header from './Header.svelte'
+  import './styles.css'
+
+  onNavigate(() => {
+    if (!('startViewTransition' in document)) {
+      return
+    }
+
+    return new Promise((resolve) => document.startViewTransition(() => new Promise(resolve)))
+  })
 </script>
 
 <div class="app">
-	<Header />
+  <Header />
 
-	<main>
-		<slot />
-	</main>
+  <main>
+    <slot />
+  </main>
 
-	<footer>
-		<p>Тестировние: sveltekit, ci/cd, nginx, https</p>
-	</footer>
+  <footer>
+    <p>Тестировние: sveltekit, ci/cd, nginx, https</p>
+  </footer>
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    max-width: 64rem;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 12px;
+  }
 
-	footer a {
-		font-weight: bold;
-	}
+  footer a {
+    font-weight: bold;
+  }
 
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+  @media (min-width: 480px) {
+    footer {
+      padding: 12px 0;
+    }
+  }
 </style>
